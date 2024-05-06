597eccf32f75c7eedbe4e7108be39967c631d1e9
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2024-05-06 14:27:16 -0300

    feat: add atualization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
+@Controller()//dacoreters, aplicado num método, ele é uma função que recebe algo como parametro, e decolve algo
+export class AppController { //passa essa clase como parametro
   constructor(private readonly appService: AppService) {}
 
   @Get()
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
+@Module({ //raiz do projeto
   imports: [],
   controllers: [AppController],
   providers: [AppService],
